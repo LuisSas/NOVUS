e7f9d300da5de482e849f031779584433f59b4c7
LuisSas <86890222+LuisSas@users.noreply.github.com>
2021-10-11 17:05:43 -0500

    Update get_mac_adress.ino

--- get_mac_adress/get_mac_adress.ino
+++ get_mac_adress/get_mac_adress.ino
@@ -1,9 +1,11 @@
-#include "WiFi.h"
+//Programa de Arduino IDE para la obtencion de la dirección MAC del esp32
+
+#include "WiFi.h" //Librería para uso de WIFI
  
 void setup(){
-  Serial.begin(115200);
-  WiFi.mode(WIFI_MODE_STA);
-  Serial.println(WiFi.macAddress());
+  Serial.begin(115200); //Puerto Serial a 115200 boudios
+  WiFi.mode(WIFI_MODE_STA);  //ESP en modo de estación wifi
+  Serial.println(WiFi.macAddress()); //Imprimir dirección MAC
 }
  
 void loop(){
